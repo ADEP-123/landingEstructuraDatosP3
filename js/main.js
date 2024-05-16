50cdd86a64afdfbbff5cdef26b2b2621b939408d
Sneatx <santi02005@hotmail.com>
2024-05-15 19:11:19 -0500

    feat: :sparkles: Modulo para agregar la data del recuadro terminado

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 let cuadroAbout = document.querySelector(".main__about")
 let btnAbout = document.getElementById("buttonAbout")
+let imgCajero = document.querySelector(".cajero")
+let imgHanoi = document.querySelector(".hanoi")
 let main = document.querySelector(".main")
 
 btnAbout.addEventListener("click", e => {
     e.preventDefault()
     main.classList.add("blur")
     cuadroAbout.style.display = "flex"
+    let strHTML =  `<p>Aqui tenemos que poner el html de nosotros jeje</p>`
+    generarRecuadro(strHTML)
+});
+
+imgCajero.addEventListener("click", e => {
+    e.preventDefault()
+    main.classList.add("blur")
+    cuadroAbout.style.display = "flex"
+    let strHTML =  `<p>Aqui tenemos que poner el html e ejercicio 1</p>`
+    generarRecuadro(strHTML)
+});
+
+imgHanoi.addEventListener("click", e => {
+    e.preventDefault()
+    main.classList.add("blur")
+    cuadroAbout.style.display = "flex"
+    let strHTML =  `<p>Aqui tenemos que poner el html e ejercicio 2</p>`
+    generarRecuadro(strHTML)
 });
 
 let closeButton = document.getElementById("closeButton")
@@ -14,3 +34,7 @@ closeButton.addEventListener("click", e=>{
     main.classList.remove("blur")
     cuadroAbout.style.display = "none"
 })
+
+function generarRecuadro(data){
+    document.querySelector('.about__container').innerHTML = data
+}
