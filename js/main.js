43920cd00b0477ea309baccfa33cdae255a4116e
Elpanchez <santiagosanchez.ribero@gmail.com>
2024-05-17 14:59:48 -0500

    feat: :sparkles: Se añadió una breve descripción para cada uno de los ejercicios

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ btnAbout.addEventListener("click", e => {
 
 imgCajero.addEventListener("click", e => {
     e.preventDefault()
-    let strHTML =  `<p>Aqui tenemos que poner el html e ejercicio 1</p>
+    let strHTML =  `<p>El Problema de la Cola del Banco consiste en simular la dinámica de una fila de clientes que esperan ser atendidos por un cajero en un banco. Cada cliente llega a la cola en un momento determinado y espera su turno para ser atendido. El cajero atiende a los clientes en el orden en que llegaron, siguiendo el principio de "primero en entrar, primero en salir" (FIFO). <br>
+    Para el desarrollo de este ejercicio se uso html y css para el frontend y js para el backend.</p>
     <button id="buttonCajero">Ir a Página</button>`
     generarRecuadro(strHTML)
     redireccionButton("buttonCajero", 'https://www.youtube.com/watch?v=Z3J_MCbwaJ0') 
@@ -20,7 +21,13 @@ imgCajero.addEventListener("click", e => {
 
 imgHanoi.addEventListener("click", e => {
     e.preventDefault()
-    let strHTML =  `<p>Aqui tenemos que poner el html e ejercicio 2</p>
+    let strHTML =  `<p>El ejercicio de la Torre de Hanoi se basa en una leyenda india sobre el fin del mundo, donde monjes deben mover 64 discos de oro siguiendo reglas específicas. El matemático francés Édouard Lucas en 1883, basandose en esta leyenda crea este rompecabezas de tres varillas y discos de madera que enseña principios de algoritmos y estructuras de datos. Según la leyenda, tomaría más de 500 billones de siglos completar la tarea, simbolizando la eternidad. <br>
+    Reglas: <br>
+    1. Sólo se puede mover un disco cada vez. <br>
+
+    2. Un disco de mayor tamaño no puede descansar sobre uno más pequeño que él mismo. <br>
+
+    3. Sólo puedes desplazar el disco que se encuentre arriba en cada varilla.  </p>
     <button id="buttonHanoi">Ir a Página</button>`
     generarRecuadro(strHTML)
     redireccionButton("buttonHanoi", 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
