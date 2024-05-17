c5021a268db65de545b3ecae76b19fcd868a7787
ADEP-123 <andep210399@gmail.com>
2024-05-17 16:52:32 -0500

    feat: :sparkles: tarjeta ejercicio 1

--- js/main.js
+++ js/main.js
@@ -6,22 +6,25 @@ let main = document.querySelector(".main")
 
 btnAbout.addEventListener("click", e => {
     e.preventDefault()
-    let strHTML =  `<p>Aqui tenemos que poner el html de nosotros jeje</p>`
+    let strHTML = `<p>Aqui tenemos que poner el html de nosotros jeje</p>`
     generarRecuadro(strHTML)
 });
 
 imgCajero.addEventListener("click", e => {
     e.preventDefault()
-    let strHTML =  `<p>El Problema de la Cola del Banco consiste en simular la dinámica de una fila de clientes que esperan ser atendidos por un cajero en un banco. Cada cliente llega a la cola en un momento determinado y espera su turno para ser atendido. El cajero atiende a los clientes en el orden en que llegaron, siguiendo el principio de "primero en entrar, primero en salir" (FIFO). <br>
-    Para el desarrollo de este ejercicio se uso html y css para el frontend y js para el backend.</p>
+    let strHTML =  /*html*/`<h1>Problema de la cola del banco</h1>
+    <img src="storage/img/fila.gif" alt="">
+    <p>El problema de las filas en el banco surgió de la observación de las colas de personas esperando atención en bancos, supermercados y otros establecimientos de servicios. Esta situación diaria ha sido estudiada en la teoría de colas y la gestión de operaciones para comprender y optimizar tanto el tiempo de espera como la eficiencia del servicio.
+    <br>Para entender este problema, se realizo un aplicativo web interactivo sobre el uso de cajeros electronicos, donde la persona prodra, agregar cajeros, eliminar cajeros, agregar personas a los cajeros y a la fila de espera por ser atendidos, al igual que completar transacciones (es decir, cuando la persona completa su transaccion en el cajero y sale de la fila). 
+    </p>
     <button id="buttonCajero" class="ex__button">Ir a Página</button>`
     generarRecuadro(strHTML)
-    redireccionButton("buttonCajero", 'https://www.youtube.com/watch?v=Z3J_MCbwaJ0') 
+    redireccionButton("buttonCajero", 'https://adep-123.github.io/estructuraDatosA/')
 });
 
 imgHanoi.addEventListener("click", e => {
     e.preventDefault()
-    let strHTML =  `<p>El ejercicio de la Torre de Hanoi se basa en una leyenda india sobre el fin del mundo, donde monjes deben mover 64 discos de oro siguiendo reglas específicas. El matemático francés Édouard Lucas en 1883, basandose en esta leyenda crea este rompecabezas de tres varillas y discos de madera que enseña principios de algoritmos y estructuras de datos. Según la leyenda, tomaría más de 500 billones de siglos completar la tarea, simbolizando la eternidad. <br>
+    let strHTML = `<p>El ejercicio de la Torre de Hanoi se basa en una leyenda india sobre el fin del mundo, donde monjes deben mover 64 discos de oro siguiendo reglas específicas. El matemático francés Édouard Lucas en 1883, basandose en esta leyenda crea este rompecabezas de tres varillas y discos de madera que enseña principios de algoritmos y estructuras de datos. Según la leyenda, tomaría más de 500 billones de siglos completar la tarea, simbolizando la eternidad. <br>
     Reglas: <br>
     1. Sólo se puede mover un disco cada vez. <br>
 
@@ -30,24 +33,26 @@ imgHanoi.addEventListener("click", e => {
     3. Sólo puedes desplazar el disco que se encuentre arriba en cada varilla.  </p>
     <button id="buttonHanoi" class="ex__button">Ir a Página</button>`
     generarRecuadro(strHTML)
-    redireccionButton("buttonHanoi", 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    redireccionButton("buttonHanoi", 'https://adep-123.github.io/estructuraDatosB/')
 });
 
-function generarRecuadro(data){
+function generarRecuadro(data) {
     main.classList.add("blur")
     cuadroAbout.style.display = "flex"
     document.querySelector('.about__container').innerHTML = data
 }
 
-function redireccionButton(buttonId, url){
+function redireccionButton(buttonId, url) {
     let redireccionButton = document.getElementById(buttonId);
-    redireccionButton.addEventListener("click", e=>{
+    redireccionButton.removeEventListener("click", e => { })
+    redireccionButton.addEventListener("click", e => {
         e.preventDefault()
-        window.location.href=url
+        window.location.href = url
     })
 }
+
 let closeButton = document.getElementById("closeButton")
-closeButton.addEventListener("click", e=>{
+closeButton.addEventListener("click", e => {
     e.preventDefault()
     main.classList.remove("blur")
     cuadroAbout.style.display = "none"
